4e45cd5a5b8d11bbd53090d226de0b6fe4e378e1
ArtScorpio <raichynetsa.v@gmail.com>
2024-12-05 09:29:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,2 @@
-# Конфігураційні дані
 TOKEN = "7457549035:AAGghIjMr2xSo351RskqYn45Rt2-TZgts6Q"
-ADMIN_ID = "@ArtScorpio"
-CHANNEL_ID = "@newstranscarpathia"
+CHANNEL_ID = "@newstranscarpathia"  # перевірте, чи правильний ID каналу
